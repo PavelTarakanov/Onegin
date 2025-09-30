b4e12c3e941fc4efddee25312925abb8c9aebef0
PavelTarakanov <t.p.d.23102007@mail.ru>
2025-09-30 16:09:31 +0300

    First upload

---
+++ Read_Text.cpp
@@ -0,0 +1,289 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include "Read_Text.h"
+
+
+int main(int argc, char* argv[])
+{
+    char** text = NULL;
+    FILE* input_address = NULL;
+    FILE* output_address = NULL;
+    struct stat statistics = {};
+    char* buffer = NULL;
+    int number_of_str = 0;
+
+    check_file_founded(argc, argv[0]);
+    check_file_opening(argv[1], &input_address);
+    check_file_opening(argv[2], &output_address);
+
+    fstat(fileno(input_address), &statistics);
+//    printf("%ld\n", statistics.st_size);
+    buffer = (char*) calloc(statistics.st_size, sizeof(char));
+    fread(buffer, sizeof(char), statistics.st_size, input_address);
+    /*
+    for (int i = 0; i < statistics.st_size; i++)
+    {
+        putchar(buffer[i]);
+    }
+    */
+    number_of_str = str_counter(buffer);
+    text = (char**) calloc(number_of_str, sizeof(char*));
+    make_indicator_massive(text, buffer);
+
+    booble_sort(text, number_of_str, my_strcmp);
+    output_text(output_address, text, number_of_str);
+
+    booble_sort(text, number_of_str, my_reverse_strcmp);
+    output_text(output_address, text, number_of_str);
+
+    //printf("%d", my_reverse_strcmp("abc\n", "abcd*&%^**\n"));
+    booble_sort(text, number_of_str, my_indicator_cmp);
+    output_text(output_address, text, number_of_str);
+
+    free(buffer);
+    free(text);
+
+    check_file_closing(input_address);
+    check_file_closing(output_address);
+}
+
+void check_file_founded(int argc, const char *input_address)
+{
+    if (argc < NUMBER_OF_FILES)
+    {
+        fprintf(stderr, "Using %s input_address\n", input_address);
+    }
+
+    return;
+}
+
+void check_file_opening(const char *input_address, FILE** file_ptr)
+{
+    if ((*file_ptr = fopen(input_address, "a+")) == NULL)
+    {
+        fprintf(stderr, "Can't open file\" %s\"\n", input_address);
+    }
+
+    return;
+}
+
+void check_file_closing(FILE* input_address)
+{
+    assert(input_address);
+
+    if (fclose(input_address) != 0)
+    {
+        fprintf(stderr, "Error while closing file\n");
+    }
+
+    return;
+}
+
+void make_indicator_massive(char* text[], char* buffer)
+{
+    assert(text);
+    assert(buffer);
+
+    char* str_address = NULL;
+    int str_number = 0;
+
+    text[str_number] = buffer;
+    str_number++;
+    str_address = strchr(buffer, '\n');
+    assert(str_address);
+    while (str_address != NULL)
+    {
+        text[str_number] = str_address+1;
+        //printf("%d\n", str_number);
+        str_number++;
+        str_address = strchr(str_address+1, '\n');
+    }
+
+    return;
+}
+
+void booble_sort(char* text[], int number_of_str, int comparison(const char* str_1, const char* str_2))
+{
+    assert(text);
+
+    char* swp = 0;
+
+    for (int i = 0; i < number_of_str; i++)
+        for (int j = 0; j < number_of_str; j++)
+        {
+            //printf("%d %d\n", i, j);
+            if (comparison(text[i], text[j]) < 0)
+            {
+                swp = text[i];
+                text[i] = text[j];
+                text[j] = swp;
+            }
+        }
+
+    return;
+}
+
+int my_strcmp(const char* str_1, const char* str_2)
+{
+    assert(str_1);
+    assert(str_2);
+
+    int i = 0;
+    int j = 0;
+
+    while (str_1[i] != '\n' && str_2[j] != '\n')
+    {
+        if (!isalpha(str_1[i]))
+        {
+            i++;
+            continue;
+        }
+        else if (!isalpha(str_2[j]))
+        {
+            j++;
+            continue;
+        }
+        else if (tolower(str_1[i]) > tolower(str_2[j]))
+        {
+            return 1;
+        }
+        else if(tolower(str_1[i]) < tolower(str_2[j]))
+        {
+            return -1;
+        }
+
+        i++;
+        j++;
+    }
+
+    return 0;
+}
+
+int my_reverse_strcmp(const char* str_1, const char* str_2)
+{
+    assert(str_1);
+    assert(str_2);
+
+    size_t i = 0;
+    size_t j = 0;
+
+    while (str_1[i] != '\n')
+    {
+        i++;
+    }
+
+    while (str_2[j] != '\n')
+    {
+        j++;
+    }
+
+    while (i > 0 && j > 0)
+    {
+        if (!isalpha(str_1[i]))
+        {
+            i--;
+            continue;
+        }
+        else if (!isalpha(str_2[j]))
+        {
+            j--;
+            continue;
+        }
+        else if (tolower(str_1[i]) > tolower(str_2[j]))
+        {
+            return 1;
+        }
+        else if(tolower(str_1[i]) < tolower(str_2[j]))
+        {
+            return -1;
+        }
+
+        i--;
+        j--;
+    }
+
+    if (i == 0 && j == 0)
+    {
+        return 0;
+    }
+    else if(i == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int my_indicator_cmp(const char* ind_1, const char* ind_2)
+{
+    assert(ind_1);
+    assert(ind_2);
+
+    return (int) (ind_1 - ind_2);
+}
+
+size_t my_strlen(const char* str)
+{
+    assert(str);
+
+    int i = 0;
+    size_t count = 0;
+    while (1)
+    {
+        if (*(str+i) == '\0' || *(str+i) == '\n')
+        {
+            return count;
+        }
+        count++;
+        i++;
+    }
+}
+
+void output_text(FILE* output_address, char* text[], int number_of_str)
+{
+    assert(output_address);
+    assert(text);
+
+    for (int i = 0; i < number_of_str; i++)
+        for (int j = 0; j < MAX_STR_LENGTH; j++)
+        {
+            if (text[i][j] != '\n')
+                putc(text[i][j], output_address);
+            else
+            {
+                putc('\n', output_address);
+                break;
+            }
+        }
+
+    fprintf(output_address, "\n\n\n\n\n");
+
+    return;
+}
+
+int str_counter(char* buffer)
+{
+    assert(buffer);
+
+    int count = 0;
+    int i = 0;
+
+    while (*(buffer+i) != '\0')
+    {
+        if (*(buffer+i) == '\n')
+        {
+            count++;
+        }
+
+        i++;
+    }
+
+    return count;
+}
---
+++ Read_Text.h
@@ -0,0 +1,24 @@
+#ifndef READ_TEXT_H
+#define READ_TEXT_H
+
+const int NUMBER_OF_FILES = 2;
+const int MAX_STR_LENGTH = 200;
+
+void check_file_founded(int argc, const char *input_address);
+void check_file_opening(const char *input_address, FILE** file_ptr);
+void check_file_closing(FILE* input_address);
+
+void make_indicator_massive(char* text[], char* buffer);
+
+
+int str_counter(char* buffer);
+
+size_t my_strlen(const char* str);
+int my_strcmp(const char* str_1, const char* str_2);
+void booble_sort(char* text[], int number_of_str, int comparison(const char* str_1, const char* str_2));
+int my_reverse_strcmp(const char* str_1, const char* str_2);
+int my_indicator_cmp(const char* ind_1, const char* ind_2);
+
+void output_text(FILE* input_address, char* text[], int number_of_str);
+
+#endif //READ_TEXT_H
